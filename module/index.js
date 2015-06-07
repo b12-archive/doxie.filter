1454296a79ba3bdff9954997573bfd19c2f34d40
Christoph Hermann <christoph.hermann@kaba.com>
2015-06-07 11:25:32 +0200

    initial functionality :boom:

--- module/index.js
+++ module/index.js
@@ -1,1 +1,20 @@
-export default () => () => {};
+import { 
+  ifElse,
+  isFunction,
+  filter,
+  compose } from '1-liners';
+
+const castBool = val => val === true;
+
+const filterCurried = filterFn => 
+  ifElse(
+    arr => Array.isArray(arr),
+    arr => filter( compose(castBool, filterFn), arr),
+    () => { throw new Error('Filter expected an array'); });
+
+export default 
+  ifElse(
+    f => isFunction(f),
+    f => filterCurried(f),
+    () => { throw new Error('Filter expected a function'); });
+
