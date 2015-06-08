865a1835651ee0c9223013b7b56ce07d4c9d25ae
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-08 07:10:57 +0200

    Update logic to fixed test

--- module/index.js
+++ module/index.js
@@ -1,14 +1,12 @@
 const ifElse = require('1-liners/ifElse');
 const isFunction = require('1-liners/isFunction');
 const filter = require('1-liners/filter');
-const compose = require('1-liners/compose');
 
-const castBool = (val) => val === true;
 const throwError = (msg) => () => { throw new Error(msg); };
 
 const filterCurried = (filterFn) => ifElse(
   Array.isArray,
-  (arr) => filter(compose(castBool, filterFn), arr),
+  (arr) => filter(filterFn, arr),
   throwError('Filter expected an array')
 );
 
